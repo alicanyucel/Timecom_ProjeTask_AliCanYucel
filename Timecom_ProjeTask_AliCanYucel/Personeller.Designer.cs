05dc90679429035a73c2099ffdc83a75af3b3e24
Ali Can Yücel <32604562+alicanyucel@users.noreply.github.com>
2022-08-21 21:36:30 +0300

    Sayfaların Kodlanması Kaldı .Ekeranlar Ok.

--- Timecom_ProjeTask_AliCanYucel/Personeller.Designer.cs
+++ Timecom_ProjeTask_AliCanYucel/Personeller.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Personeller));
             this.btnPersonelAra = new System.Windows.Forms.Button();
             this.btnPersonelDuzenle = new System.Windows.Forms.Button();
@@ -36,6 +37,25 @@
             this.btnGeriGel = new System.Windows.Forms.Button();
             this.btnHesaplamayaGit = new System.Windows.Forms.Button();
             this.btnAnaSayfa = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtPersonelKodu = new System.Windows.Forms.TextBox();
+            this.txtPersonelAd = new System.Windows.Forms.TextBox();
+            this.txtPersonelSoyad = new System.Windows.Forms.TextBox();
+            this.txtPersonelTcNo = new System.Windows.Forms.TextBox();
+            this.cbxCinsiyet = new System.Windows.Forms.ComboBox();
+            this.txtIseBaslamaTarihi = new System.Windows.Forms.TextBox();
+            this.cbxEngellilikDurumu = new System.Windows.Forms.ComboBox();
+            this.cbxCalismaSekli = new System.Windows.Forms.ComboBox();
+            this.cbxUcreti = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // btnPersonelAra
@@ -104,6 +124,161 @@
             this.btnAnaSayfa.UseVisualStyleBackColor = true;
             this.btnAnaSayfa.Click += new System.EventHandler(this.btnAnaSayfa_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Personel Kodu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Cinsiyet";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "İşe Başlama Tarihi";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Personel TcNo";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 65);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Personel Ad";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 96);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Personel Soyad";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(29, 247);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Engellilik Durumu";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(29, 284);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Çalışma Şekli";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(29, 322);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Ücreti";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtPersonelKodu
+            // 
+            this.txtPersonelKodu.Location = new System.Drawing.Point(114, 36);
+            this.txtPersonelKodu.Name = "txtPersonelKodu";
+            this.txtPersonelKodu.Size = new System.Drawing.Size(100, 20);
+            this.txtPersonelKodu.TabIndex = 17;
+            // 
+            // txtPersonelAd
+            // 
+            this.txtPersonelAd.Location = new System.Drawing.Point(114, 62);
+            this.txtPersonelAd.Name = "txtPersonelAd";
+            this.txtPersonelAd.Size = new System.Drawing.Size(100, 20);
+            this.txtPersonelAd.TabIndex = 18;
+            // 
+            // txtPersonelSoyad
+            // 
+            this.txtPersonelSoyad.Location = new System.Drawing.Point(114, 93);
+            this.txtPersonelSoyad.Name = "txtPersonelSoyad";
+            this.txtPersonelSoyad.Size = new System.Drawing.Size(100, 20);
+            this.txtPersonelSoyad.TabIndex = 19;
+            // 
+            // txtPersonelTcNo
+            // 
+            this.txtPersonelTcNo.Location = new System.Drawing.Point(114, 130);
+            this.txtPersonelTcNo.MaxLength = 11;
+            this.txtPersonelTcNo.Name = "txtPersonelTcNo";
+            this.txtPersonelTcNo.Size = new System.Drawing.Size(100, 20);
+            this.txtPersonelTcNo.TabIndex = 20;
+            // 
+            // cbxCinsiyet
+            // 
+            this.cbxCinsiyet.FormattingEnabled = true;
+            this.cbxCinsiyet.Location = new System.Drawing.Point(93, 169);
+            this.cbxCinsiyet.Name = "cbxCinsiyet";
+            this.cbxCinsiyet.Size = new System.Drawing.Size(121, 21);
+            this.cbxCinsiyet.TabIndex = 21;
+            // 
+            // txtIseBaslamaTarihi
+            // 
+            this.txtIseBaslamaTarihi.Location = new System.Drawing.Point(128, 205);
+            this.txtIseBaslamaTarihi.MaxLength = 11;
+            this.txtIseBaslamaTarihi.Name = "txtIseBaslamaTarihi";
+            this.txtIseBaslamaTarihi.Size = new System.Drawing.Size(100, 20);
+            this.txtIseBaslamaTarihi.TabIndex = 22;
+            // 
+            // cbxEngellilikDurumu
+            // 
+            this.cbxEngellilikDurumu.FormattingEnabled = true;
+            this.cbxEngellilikDurumu.Location = new System.Drawing.Point(128, 244);
+            this.cbxEngellilikDurumu.Name = "cbxEngellilikDurumu";
+            this.cbxEngellilikDurumu.Size = new System.Drawing.Size(121, 21);
+            this.cbxEngellilikDurumu.TabIndex = 23;
+            // 
+            // cbxCalismaSekli
+            // 
+            this.cbxCalismaSekli.FormattingEnabled = true;
+            this.cbxCalismaSekli.Location = new System.Drawing.Point(128, 281);
+            this.cbxCalismaSekli.Name = "cbxCalismaSekli";
+            this.cbxCalismaSekli.Size = new System.Drawing.Size(121, 21);
+            this.cbxCalismaSekli.TabIndex = 24;
+            // 
+            // cbxUcreti
+            // 
+            this.cbxUcreti.FormattingEnabled = true;
+            this.cbxUcreti.Location = new System.Drawing.Point(70, 319);
+            this.cbxUcreti.Name = "cbxUcreti";
+            this.cbxUcreti.Size = new System.Drawing.Size(179, 21);
+            this.cbxUcreti.TabIndex = 25;
+            // 
             // Personeller
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -111,6 +286,24 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(934, 450);
+            this.Controls.Add(this.cbxUcreti);
+            this.Controls.Add(this.cbxCalismaSekli);
+            this.Controls.Add(this.cbxEngellilikDurumu);
+            this.Controls.Add(this.txtIseBaslamaTarihi);
+            this.Controls.Add(this.cbxCinsiyet);
+            this.Controls.Add(this.txtPersonelTcNo);
+            this.Controls.Add(this.txtPersonelSoyad);
+            this.Controls.Add(this.txtPersonelAd);
+            this.Controls.Add(this.txtPersonelKodu);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnAnaSayfa);
             this.Controls.Add(this.btnHesaplamayaGit);
             this.Controls.Add(this.btnGeriGel);
@@ -121,6 +314,7 @@
             this.Name = "Personeller";
             this.Text = "Timecom Personeller";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -133,5 +327,24 @@
         private System.Windows.Forms.Button btnGeriGel;
         private System.Windows.Forms.Button btnHesaplamayaGit;
         private System.Windows.Forms.Button btnAnaSayfa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtPersonelKodu;
+        private System.Windows.Forms.TextBox txtPersonelAd;
+        private System.Windows.Forms.TextBox txtPersonelSoyad;
+        private System.Windows.Forms.TextBox txtPersonelTcNo;
+        private System.Windows.Forms.ComboBox cbxCinsiyet;
+        private System.Windows.Forms.TextBox txtIseBaslamaTarihi;
+        private System.Windows.Forms.ComboBox cbxEngellilikDurumu;
+        private System.Windows.Forms.ComboBox cbxCalismaSekli;
+        private System.Windows.Forms.ComboBox cbxUcreti;
     }
 }
